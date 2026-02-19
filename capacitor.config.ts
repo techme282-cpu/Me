a8b4205c0fbfb2af0f63ada5a11872f2b5b369ee
techme282-cpu <techme282@gmail.com>
2026-02-19 08:31:07 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.57a5870371d34794a6d6398931acd9e6',
-  appName: 'INCONNU BOY',
+  appId: 'com.purgehub.app',
+  appName: 'Purge Hub',
   webDir: 'dist',
   server: {
     url: 'https://57a58703-71d3-4794-a6d6-398931acd9e6.lovableproject.com?forceHideBadge=true',
