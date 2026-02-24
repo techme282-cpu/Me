8f0d006f9d0631fd16c20cc548793a446b5ff755
INCONNU BOY <vtry813@gmail.com>
2026-02-24 00:23:42 -0300

    INCONNU BOY TECH 🍂

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.57a5870371d34794a6d6398931acd9e6',
-  appName: 'INCONNU BOY',
+  appId: 'com.purgehub.app',
+  appName: 'Purge Hub',
   webDir: 'dist',
   server: {
-    url: 'https://57a58703-71d3-4794-a6d6-398931acd9e6.lovableproject.com?forceHideBadge=true',
+    url: 'https://purge-hub-social-einm.vercel.app',
     cleartext: true,
   },
 };
